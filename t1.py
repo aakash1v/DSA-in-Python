561144fc1114f7d39c5848bc4ddaf333c8ad7f07
Aakash Kumar <aakashkumar113450@gmail.com>
2024-04-15 23:45:37 +0530

    improving and making efficient t1

--- t1.py
+++ t1.py
@@ -6,6 +6,19 @@ def findSum(n):
         sum +=i
     return sum
 
+# efficient program to find sum 
+def findSum(n):
+    return n*(n+1)//2
+
+
+#avoid overflow if the result is going to be
+# within limits.
+def findSum(n):
+    if n%2==0:
+        return n//2 * (n+1)
+    else:
+        return ((n+1)//2 )*n
+
 
 #Driver Code
 n =5
