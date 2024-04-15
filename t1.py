bba76ab58f85c30ec7cc452b019a6b218032157f
Aakash Kumar <aakashkumar113450@gmail.com>
2024-04-15 23:40:38 +0530

    t1 version 1

--- t1.py
+++ t1.py
@@ -1,1 +1,12 @@
-print('hello world1')
+'''Given a number n, find the sum of first natural numbers.
+'''
+def findSum(n):
+    sum =0
+    for i in range(1,n+1):
+        sum +=i
+    return sum
+
+
+#Driver Code
+n =5
+print(findSum(n))
